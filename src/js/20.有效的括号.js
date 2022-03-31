c4c8fdd9af21c7666e035291692a2d50006f085a
AprilTong <1317795103@qq.com>
2022-03-31 21:23:25 +0800

    feat:删除有序数组重复项

--- src/js/20.有效的括号.js
+++ src/js/20.有效的括号.js
@@ -9,11 +9,32 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
+var isValid2 = function (s) {
     if (s.length % 2 !== 0) return false;
     while (s.indexOf('()') > -1 || s.indexOf('[]') > -1 || s.indexOf('{}') > -1) {
         s = s.replace(/\(\)/, '').replace(/\{\}/g, '').replace(/\[\]/g, '');
     }
     return !s;
+};
+var isValid = function (s) {
+    if (!s) return true;
+    if (s.length % 2 !== 0) return false;
+    let arr = [];
+    let map = new Map([
+        [')', '('],
+        ['}', '{'],
+        [']', '['],
+    ]);
+    for (let v of s) {
+        if (!map.has(v)) {
+            arr.push(v);
+        } else {
+            if (!arr.length || arr[arr.length - 1] !== map.get(v)) {
+                return false;
+            }
+            arr.pop();
+        }
+    }
+    return arr.length === 0;
 };
 // @lc code=end
